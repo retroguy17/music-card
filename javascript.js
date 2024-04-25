104fabed862d5069b8a1f04fbfc1d1f261a0ef0d
retroguy17 <vickyeswar1730@gmail.com>
2024-04-25 15:41:31 +0530

    file exported

--- javascript.js
+++ javascript.js
@@ -1,7 +1,13 @@
-let container=document.querySelectorAll("container");
+import {music} from "../music card/song details/song_det"
 
+
+console.log(music[0])
+let container=document.querySelectorAll("container");
+ 
 let song_name=document.querySelectorAll("track_name");
 let seek_slider=document.querySelectorAll("seek_slider");
+let track_index=0
+let image=document.getElementById("image");
 
-let 
-
+let td=document.createElement("img")
+td.src=music[].song_img
---
+++ song details/song_det.js
@@ -0,0 +1,16 @@
+
+
+
+export let music=[
+    {
+    song_name:"dandelions",
+    song:"../objects/Dandelions_320(PaglaSongs).mp3",
+    img:"../objects/eline.jpg"
+},{
+    
+}
+
+
+
+]
+
