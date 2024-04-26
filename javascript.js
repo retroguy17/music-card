1af7d75d79aaaeae2b543cacd074e720a67c9e77
retroguy17 <vickyeswar1730@gmail.com>
2024-04-26 10:47:46 +0530

    new song

--- javascript.js
+++ javascript.js
@@ -1,4 +1,23 @@
-import {music_list} from "../music card/song details/song_det.js" ;
+const music_list=[
+    {
+    song_name:"dandelions",
+    song:"../objects/Dandelions_320(PaglaSongs).mp3",
+    img:"https://www.google.com/imgres?q=dandelions%20slowed%20%2B%20reverb%20images&imgurl=https%3A%2F%2Fsource.boomplaymusic.com%2Fgroup10%2FM00%2F03%2F29%2Ffb6a444f04b04f7aba33d98b659705b1_464_464.jpg&imgrefurl=https%3A%2F%2Fwww.boomplay.com%2Fsongs%2F112300788&docid=k1Eo_kLMIpba9M&tbnid=6ABWkmYUHqfHIM&vet=12ahUKEwjhhIy3xN2FAxXjRmwGHa5MAa0QM3oECF8QAA..i&w=464&h=464&hcb=2&ved=2ahUKEwjhhIy3xN2FAxXjRmwGHa5MAa0QM3oECF8QAA"
+},
+    {
+    song_name:"perfect",
+    song:"../objects/Darling-Just-Hld-My-Hand_320(PaglaSongs).mp3",
+    img:"https://images.genius.com/35fad95b68ee49ae50eb71a918075e75.1000x1000x1.png"
+
+},
+    {
+    song_name:"summertime sadness",
+    song:"../objects/Summertime-Sadness_320(PaglaSongs).mp3",
+    img:"../objects/pexels-ganinph-7790348.jpg"
+
+}
+]
+
 
 let container=document.querySelector(".container");
  
--- song details/song_det.js
+++ song details/song_det.js
@@ -25,4 +25,3 @@
 ]
 
 
-export default music_list
